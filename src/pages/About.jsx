6040cc1c321b65b3036ef2a09393e8a39a456ec7
Valhalla-Turtle <Morgantasset@gmail.com>
2025-03-19 14:34:33 +0000

    ajustement collapse

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -8,7 +8,8 @@ function About() {
     <div className="background">
       <Header />
       <Banner image={"./banner2.png"} />
-      <Collapsible title={'titre'} contente={'contenu du bouton a la con'}/>
+      <Collapsible title={'Fiabilité'} contente={'Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.'}/>
+      <Collapsible title={'Respect'} contente={'La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.'} />
     </div>
   );
 }
